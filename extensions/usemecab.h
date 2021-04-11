300d826e3c04f6b8eed6971e51c195b3e3f5a7f7
yashichi05 <39804690+yashichi05@users.noreply.github.com>
2021-04-11 18:03:50 +0800

    Update usemecab.h

--- extensions/usemecab.h
+++ extensions/usemecab.h
@@ -23,7 +23,7 @@ public:
         mecab_destroy(mecab);
     };
     const char *char_sentence;
-    std::wstring translate_sentence = L"";
+    std::wstring translate_sentence = L"\x200b \n";
     const mecab_node_t *node_sentence;
 
 private:
@@ -34,7 +34,7 @@ private:
         QStringList text_ary = input_qs.split(QString(S(L"\x200b \n")));
         QString origin_text = text_ary[0];
         if (text_ary.size() > 1)
-            translate_sentence = text_ary[1].toStdWString();
+            translate_sentence += text_ary[1].toStdWString();
         char *char_s = new char[origin_text.toStdString().length() + 1];
         strcpy(char_s, origin_text.toStdString().c_str());
         char_sentence = char_s;
@@ -47,9 +47,8 @@ private:
     void outputHtml(std::wstring &sentence, int fontSize)
     {
 
-        QString style = "<style>td{padding:0 1px;}</style>";
-        QString tr1 = "";
-        QString tr2 = "";
+        QString style = "<style>span{float:left;}</style>";
+        QString content = "";
         for (; node_sentence; node_sentence = node_sentence->next)
         {
             if (node_sentence->stat == MECAB_NOR_NODE || node_sentence->stat == MECAB_UNK_NODE)
@@ -61,12 +60,10 @@ private:
                 QString pronounce(node_sentence->feature);
                 QStringList pronounce_list = pronounce.split(QString(","));
 
-                tr1 += "<td align=\"center\" style=\"font-size:" + QString::number(fontSize * 0.75) + "px;\">" + pronounce_list.takeLast() + "</td>";
-                tr2 += "<td align=\"center\" style=\"white-space:nowrap;\">" + current_str + "</td>";
+                content += "<span><div style=\"font-size:" + QString::number(fontSize * 0.75) + "px;\">" + pronounce_list.takeLast() + "</div>";
+                content += "<div style=\"white-space:nowrap;\">" + current_str + "</div></span>";
             }
         }
-        tr1 = "<tr align=\"center\">" + tr1 + "</tr>";
-        tr2 = "<tr align=\"center\">" + tr2 + "</tr>";
-        sentence = (style + "<table>" + tr1 + tr2 + "</table>").toStdWString();
+        sentence = (style + content).toStdWString();
     }
 };
