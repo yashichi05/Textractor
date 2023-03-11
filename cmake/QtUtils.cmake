3e7a6064352eeb21b67aa3c8eea15d608970de47
howard <howard@richwhole.net>
2023-03-11 12:12:43 +0800

    s

--- cmake/QtUtils.cmake
+++ cmake/QtUtils.cmake
@@ -6,9 +6,8 @@ macro(msvc_registry_search)
 		# get root path so we can search for 5.3, 5.4, 5.5, etc
 		string(REPLACE "/Tools" ";" QT_BIN "${QT_BIN}")
 		
-		message(WARNING "folder:${QT_BIN}")
 		list(GET QT_BIN 0 QT_BIN)
-		if("" STREQUAL "${QT_BIN}")
+		if("/" STREQUAL "${QT_BIN}")
 			file(REAL_PATH "../../Qt" QT_BIN)
 		endif()
 		file(GLOB QT_VERSIONS "${QT_BIN}/5.1*")
