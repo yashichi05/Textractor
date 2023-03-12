d8635c96340a00ac37267bb24f0d4381e2075aa5
howard <howard@richwhole.net>
2023-03-12 20:26:07 +0800

    s

--- extensions/usemecab.h
+++ extensions/usemecab.h
@@ -74,7 +74,11 @@ private:
                 current_str.truncate(node_sentence->length / 3);
                 QString pronounce(node_sentence->feature);
                 QStringList pronounce_list = pronounce.split(QString(","));
-                totalPronounce += pronounce_list.takeLast();
+                if(pronounce_list.takeLast() == "*") {
+                    totalPronounce += current_str;
+                } else {
+                    totalPronounce += pronounce_list.takeLast();
+                }
                 QString topText = "<td align=\"center\" style=\"font-size:" + QString::number(kanaSize == 0?(fontSize * 0.75):kanaSize) + "px;\">" + pronounce_list.takeLast() + "</td>";
                 QString mainText = "<td align=\"center\">" + current_str + "</td>";
 
